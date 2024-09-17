727a7179a0f5614af1cdd6a6ee43c440e1e538d0
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-09-17 17:32:30 +0100

    Setting up everything to proceed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
+import { TypeOrmModule } from '@nestjs/typeorm'; // TypeOrm
+
 @Module({
   imports: [UserModule],
   controllers: [AppController],
