1d1518099f061d05f973ae760566b318964c57eb
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-09-17 17:44:48 +0100

    Created a connection between the app and postgres database but still yet to test it out

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 import { TypeOrmModule } from '@nestjs/typeorm'; // TypeOrm
+import { ConfigModule } from '@nestjs/config'; // Configuration Module
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
