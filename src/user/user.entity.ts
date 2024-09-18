12af680eff3c46a795f3779c31781c1b5309c12c
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-09-18 12:47:00 +0100

    Working on the Signup to hash and save the user

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -5,7 +5,7 @@ export class User {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
+    @Column({ default: false })
     isChef: boolean;
 
     @Column({ unique: true })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,9 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
+import * as bcrypt from "bcrypt";
+
 import { User } from './user.entity'; // User Entity
 
 @Injectable()
 export class UserService {
     constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+    createNewUser(username: string, email: string, password: string) {}
 }
