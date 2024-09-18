ac9a77c881738adf77525d3dba3d95e720336a71
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-09-18 18:30:02 +0100

    Finished working on the authorization of the app

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, UseGuards, Request } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+
+import { UserAuthGuard } from './user.guard'; // Guard for authorization | Middleware
 
 @Controller('user')
 export class UserController {
@@ -14,6 +16,12 @@ export class UserController {
     @Post('login')
     login(@Body() body: CreateUserDto){
         return this.userService.loginUser(body.email, body.password);
+    }
+
+    @UseGuards(UserAuthGuard)
+    @Get("profile")
+    userProfile(@Request() req) {
+        return req.user;
     }
 
 }
